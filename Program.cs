1d104482a0d915b7fa4fc7a19471621272c5a1df
Thomas Herber <s2110307015@students.fh-hagenberg.at>
2022-09-30 19:53:43 +0200

    first version

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+int n = 5;
+
+int[] fibonacci = CreateFibonacciSeries(n);
+
+for (int i = 0; i < fibonacci.Length; i++) {
+  Console.WriteLine($"f_{i} = {fibonacci[i]}");
+}
+
+
+
+static int[] CreateFibonacciSeries(int count) {
+  int[] series = new int[count];
+  for (int i = 0; i < count; i++)
+    series[i] = CreateFibonacciNumber(i);
+  return series;
+}
+
+static int CreateFibonacciNumber(int index) {
+  return index switch {
+    0 => 0,
+    1 => 1,
+    _ => CreateFibonacciNumber(index - 1) + CreateFibonacciNumber(index - 2)
+  };
+}
